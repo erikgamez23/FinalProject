bdc1d934ca3e70f1b08f86f0cf875b65a75ee5e5
erikg <egamez>
2021-12-08 14:17:23 -0500

    test

--- app/src/main/java/com/c323FinalProject/egameztatclend/MainActivity.java
+++ app/src/main/java/com/c323FinalProject/egameztatclend/MainActivity.java
@@ -3,6 +3,7 @@ package com.c323FinalProject.egameztatclend;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView r = findViewById(R.id.textview1);
+        r.setText("GitHub Test");
     }
 }
