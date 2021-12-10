7bf9a5749191738510acffb0aa88ff738cb902b6
erikg <egamez>
2021-12-09 20:01:13 -0500

    NavBarActivity

--- app/src/main/java/com/c323FinalProject/egameztatclend/MainActivity.java
+++ app/src/main/java/com/c323FinalProject/egameztatclend/MainActivity.java
@@ -2,7 +2,10 @@ package com.c323FinalProject.egameztatclend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,13 @@ public class MainActivity extends AppCompatActivity {
         r.setText("GitHub Test");
         r.setText("GitHub Test 2");
         r.setText("Test 3");
+
+        Button navBar = findViewById(R.id.button);
+        navBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, NavBarActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/c323FinalProject/egameztatclend/NavBarActivity.java
+++ app/src/main/java/com/c323FinalProject/egameztatclend/NavBarActivity.java
@@ -1,14 +1,102 @@
 package com.c323FinalProject.egameztatclend;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class NavBarActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class NavBarActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
+
+    Toolbar toolbar;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    FrameLayout frameLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav_bar);
+        initializeViews();
+        initializeDefaultFragment(savedInstanceState, 0);
+        toggleDrawer();
+    }
+
+    private void initializeViews() {
+        navigationView = findViewById(R.id.navigation_view_id);
+        drawerLayout = findViewById(R.id.drawer_layout_id);
+        toolbar = findViewById(R.id.toolbar_id);
+        setSupportActionBar(toolbar);
+        frameLayout = findViewById(R.id.framelayout_id);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /**
+     * Checks if the savedInstanceState is null - onCreate() is ran
+     * If so, display fragment of navigation drawer menu at position itemIndex and
+     * set checked status as true
+     * @param savedInstanceState
+     * @param itemIndex
+     */
+    private void initializeDefaultFragment(Bundle savedInstanceState, int itemIndex){
+        if (savedInstanceState == null){
+            MenuItem menuItem = navigationView.getMenu().getItem(itemIndex).setChecked(true);
+            onNavigationItemSelected(menuItem);
+        }
+    }
+
+    /**
+     * Creates an instance of the ActionBarDrawerToggle class:
+     * 1) Handles opening and closing the navigation drawer
+     * 2) Creates a hamburger icon in the toolbar
+     * 3) Attaches listener to open/close drawer on icon clicked and rotates the icon
+     */
+    private void toggleDrawer() {
+        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+    }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.ModeOfTraining:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id, new ModeOfTrainingFragment())
+                        .commit();
+                closeDrawer();
+                break;
+            case R.id.Exercise:
+                getSupportFragmentManager().beginTransaction().replace(R.id.framelayout_id, new ExercisesFragment())
+                        .commit();
+                closeDrawer();
+                break;
+        }
+        deSelectCheckedState();
+        return true;
+    }
+    /* Checks if the navigation drawer is open - if so, close it
+     */
+    private void closeDrawer(){
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }
+    }
+
+    /**
+     * Iterates through all the items in the navigation menu and deselects them:
+     * removes the selection color
+     */
+    private void deSelectCheckedState(){
+        int noOfItems = navigationView.getMenu().size();
+        for (int i=0; i<noOfItems;i++){
+            navigationView.getMenu().getItem(i).setChecked(false);
+        }
     }
 }
