413f8e9222001683d68e0b2b0bb5493d8da9422b
erikg <egamez>
2021-12-16 18:08:12 -0500

    added calendarview

--- app/src/main/java/com/c323FinalProject/egameztatclend/CalendarFragment.java
+++ app/src/main/java/com/c323FinalProject/egameztatclend/CalendarFragment.java
@@ -7,16 +7,21 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+
 public class CalendarFragment extends Fragment {
-
-    public CalendarFragment() {
-        // Required empty public constructor
-    }
-
+    MaterialCalendarView calendar;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View v =  inflater.inflate(R.layout.fragment_calendar, container, false);
+        initializeViews(v);
+        return v;
+    }
+
+    private void initializeViews(View v) {
+        calendar = v.findViewById(R.id.materialCalendar);
+//        calendar.setOnClickListener();
     }
 }
