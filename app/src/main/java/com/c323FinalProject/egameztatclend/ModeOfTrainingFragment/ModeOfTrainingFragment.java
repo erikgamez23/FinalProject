cfee5459f3d2bcd05d129a37f2cd4a6a30ecb86a
erikg <egamez>
2021-12-16 14:35:18 -0500

    added ModeDB attributes

--- app/src/main/java/com/c323FinalProject/egameztatclend/ModeOfTrainingFragment/ModeOfTrainingFragment.java
+++ app/src/main/java/com/c323FinalProject/egameztatclend/ModeOfTrainingFragment/ModeOfTrainingFragment.java
@@ -7,6 +7,7 @@ import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RadioGroup;
 
 import com.c323FinalProject.egameztatclend.R;
 import com.google.android.material.timepicker.MaterialTimePicker;
@@ -19,17 +20,18 @@ public class ModeOfTrainingFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_mode_of_training, container, false);
-        initializeViews();
+        initializeViews(v);
         return v;
     }
 
-    private void initializeViews() {
+    private void initializeViews(View v) {
         //TODO set OnClick for set alarm on/off button
 
         //TODO set up timepicker to be able to set the alarm
 
         //TODO save option button
 
-        //TODO
+        //TODO radio
+        RadioGroup radioGroup = v.findViewById(R.id.radioGroup);
     }
 }
